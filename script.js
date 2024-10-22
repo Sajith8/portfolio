66dafdb21c68cd0156a8d66c814a3f4f5ff67ff9
Sajith8 <sajithprabakaran8@gmail.com>
2024-10-22 21:23:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  sendMail();
+});
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_to645jr";
+  const templateID = "template_6u8lhcv";
+
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch(err => console.log(err));
+}
